0febfbed043b270bd6f19257c11896ac0f1f0f0c
Xiaotian Wu <wuxiaotian@loongson.cn>
2022-03-06 12:09:24 +0800

    Add support for LoongArch

--- ipxe/general.h
+++ ipxe/general.h
@@ -4,7 +4,7 @@
 #undef CRYPTO_80211_WPA  /* WPA Personal, authenticating with passphrase */
 
 // enable additional options
-#define NET_PROTO_IPV6  /* IPv6 protocol */
+//#define NET_PROTO_IPV6  /* IPv6 protocol */
 #define DOWNLOAD_PROTO_HTTPS  /* Secure Hypertext Transfer Protocol */
 #define DOWNLOAD_PROTO_NFS  /* Network File System Protocol */
 #define IMAGE_TRUST_CMD /* Image trust management commands */
